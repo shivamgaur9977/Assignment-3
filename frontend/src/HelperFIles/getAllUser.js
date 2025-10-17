446f72ee56064e5c119c7c02d470468a9df2197e
shivamgaur9977 <shivamgaur9977@gmail.com>
2025-10-17 11:21:54 +0530

    updaated

--- frontend/src/HelperFIles/getAllUser.js
+++ frontend/src/HelperFIles/getAllUser.js
@@ -3,7 +3,7 @@ import axios from "axios";
 let getAllUsers = async (setAllUsers, setLoading) => {
     try {
         setLoading(true);
-        let res = await axios.get("http://localhost:8080/users");
+        let res = await axios.get("https://assignment-3-rqol.onrender.com/users");
         setAllUsers(res.data);
         setLoading(false);
     } catch (err) {
